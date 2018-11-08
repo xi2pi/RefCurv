f632e2e4a2e6d9e2b869f09d3b1652eaa7e2e4b6
xi2pi <s6cnwink@uni-bonn.de>
2018-11-08 10:22:55 +0100

    0.4.2

--- src/R_model/perc_model_flexible.R
+++ src/R_model/perc_model_flexible.R
@@ -1,4 +1,4 @@
-# perc_model.R
+# perc_model_flexible.R
 # Load packages
 library(gamlss)
 library(pander)
@@ -11,25 +11,28 @@ myArgs <- commandArgs(trailingOnly = TRUE)
 # Convert to numerics
 nums = as.numeric(myArgs[1:3])
 
-# Load some data
+# Load data
 data = read.csv(myArgs[4], header = TRUE, sep = ",", encoding = "latin1")
 
-# Configuration
+# Configuration edf for L, M and S
 median_df = nums[1]
 sigma_df = nums[2]
 nu_df = nums[3]
 
+# points on or off
 points_on = myArgs[7]
 
+# graph title
 graph_title = myArgs[6]
 
-
+# axis label
 x_label = myArgs[5]
 y_label = myArgs[6]
 
 x_axis = make.names(myArgs[5])
 y_axis = make.names(myArgs[6])
 
+# data preparation steps
 data_perc_1 <- data.frame(data[,x_axis],data[,y_axis])
 
 colnames(data_perc_1)[1] = "x"
@@ -37,6 +40,7 @@ colnames(data_perc_1)[2] = "y"
 
 data_perc<-na.omit(data_perc_1)
 
+# limits
 xsteps = (max(data_perc$x) - min(data_perc$x))/10
 xlim1 = min(data_perc$x) - xsteps
 xlim2 = max(data_perc$x) + xsteps
@@ -45,13 +49,16 @@ ysteps = (max(data_perc$y) - min(data_perc$y))/10
 ylim1 = min(data_perc$y) - ysteps
 ylim2 = max(data_perc$y) + ysteps
 
+# text output
 print(myArgs[8])
 print(paste(dirname(myArgs[4]),"/percentiles.png", sep=""))
 m1 <- eval(parse(text = myArgs[8]))
 summary(m1)
 
+# label setting
 label_perc <- centiles.pred(m1, xvalues=max(data_perc$x), xname= "x", cent = c(3, 10, 25, 50, 75, 90, 97))
 
+# create png
 png(paste(dirname(myArgs[4]),"/percentiles.png", sep=""))
 centiles(m1,
          xvar = data_perc$x,
@@ -83,15 +90,19 @@ text(1.05*max(data_perc$x), label_perc$C97, "P97", cex = 0.7)
 
 dev.off()
 
-
+# LMS Chart
+# x values for chart
 x_values <- seq(round(min(data_perc$x),1), round(max(data_perc$x),1), length.out = 100)
 
+# LMS values
 lms_values <- predictAll(m1, newdata=data.frame(x=x_values))
+# percentile values
 centile_values <- centiles.pred(m1, xname="x", xvalues=x_values, cent = c(3, 10, 25, 50, 75, 90, 97))
 
-
+# residuals
 resid_m1 <- centiles.pred(m1, xname="x", xvalues=data_perc$x, yval=data_perc$y, type="z-scores")
 
+# saving
 chart <- data.frame(lms_values, centile_values)
 write.csv(chart, file = paste(dirname(myArgs[4]),"/percentiles_chart.csv", sep=""))
 
