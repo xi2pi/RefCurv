ffa7ec6a198451c9647b0ad6bb0e9b14340fa381
xi2pi <s6cnwink@uni-bonn.de>
2019-10-04 12:54:15 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -110,12 +110,21 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'sphinx_rtd_theme'
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_logo = '../docs/img/refcurv_logo.png'
+html_theme_options = {
+    'display_version': False,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
